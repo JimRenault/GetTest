359fa32a126b640de7bece90737a318b81feaf24
JimRenault <1470417642@qq.com>
2021-08-16 17:03:50 +0800

    my frist commit

---
+++ test_git01/src/main/java/com/ysl/test_git01/TestGit01Application.java
@@ -0,0 +1,13 @@
+package com.ysl.test_git01;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestGit01Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TestGit01Application.class, args);
+    }
+
+}
